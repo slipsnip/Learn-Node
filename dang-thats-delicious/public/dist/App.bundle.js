ef02662208d5e3cc3715e882a09ac11a7f59157c
Michael Esch <eschmichael@yandex.com>
2019-05-02 12:01:48 -0400

    upgrade to babel 7

--- dang-thats-delicious/public/dist/App.bundle.js
+++ dang-thats-delicious/public/dist/App.bundle.js
@@ -6,9 +6,9 @@
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
@@ -100,7 +100,7 @@ exports.$$ = $$;
 /* 1 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: Couldn't find preset \"@babel/preset-env\" relative to directory \"/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/css-loader\"\n    at /home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/options/option-manager.js:293:19\n    at Array.map (<anonymous>)\n    at OptionManager.resolvePresets (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/options/option-manager.js:275:20)\n    at OptionManager.mergePresets (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/options/option-manager.js:264:10)\n    at OptionManager.mergeOptions (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/options/option-manager.js:249:14)\n    at OptionManager.init (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/options/option-manager.js:368:12)\n    at File.initOptions (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/index.js:212:65)\n    at new File (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/file/index.js:135:24)\n    at Pipeline.transform (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-core/lib/transformation/pipeline.js:46:16)\n    at transpile (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-loader/lib/index.js:46:20)\n    at Object.module.exports (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/babel-loader/lib/index.js:155:20)\n    at runLoaders (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at runSyncOrAsync (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/loader-runner/lib/LoaderRunner.js:143:3)\n    at iterateNormalLoaders (/home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/loader-runner/lib/LoaderRunner.js:232:2)\n    at /home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/loader-runner/lib/LoaderRunner.js:205:4\n    at /home/wurst/slipsnip/Learn-Node/dang-thats-delicious/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:70:14\n    at processTicksAndRejections (internal/process/task_queues.js:79:9)");
 
 /***/ }),
 /* 2 */
