0a7736dbd9e61a0026cc05c00800b54ccd60b4fb
Michael Esch <eschmichael@yandex.com>
2019-05-02 13:52:12 -0400

    use createindex

--- dang-thats-delicious/start.js
+++ dang-thats-delicious/start.js
@@ -10,8 +10,13 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
+// mongoose use new url parser and createindex
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true
+};
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE, options);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
