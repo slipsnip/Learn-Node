4b05f24df20bebb8c2e624a34ff748ad5b437d54
Michael Esch <eschmichael@yandex.com>
2019-05-03 16:09:22 -0400

    add variables.env to ignore

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  if (req.query.hasOwnProperty('name')) {
+    res.send(`Your name is ${req.query.name}`);
+  }
+  else res.send('Hey! It works!');
 });
 
 module.exports = router;
