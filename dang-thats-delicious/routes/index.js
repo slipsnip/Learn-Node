a913b696e37e55c543fc3aed3ee16b3cac797149
Michael Esch <eschmichael@yandex.com>
2019-05-13 16:57:57 -0400

    Add routes for /add POST and GET

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const storeController = require('../controllers/storeController.js');
 // Do work here
 router.get('/', storeController.homePage);
-
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 module.exports = router;
